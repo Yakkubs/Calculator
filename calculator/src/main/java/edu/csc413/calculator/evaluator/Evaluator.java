170b96aa5c98f001a9c77fb342f4948d491630bd
yakkub <yalkabsh@sfsu.edu>
2023-06-10 23:47:04 -0700

    implemented the operator and operand classes and fixed the added a method to the evaluator class that process the rest of the evaluation

--- calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
@@ -10,11 +10,17 @@ public class Evaluator {
   private Stack<Operand> operandStack;
   private Stack<Operator> operatorStack;
   private StringTokenizer expressionTokenizer;
-  private final String delimiters = " +/*-^";
+  private final String delimiters = " +/*-^()"; // added parentheses to the delimiter
 
   public Evaluator() {
     operandStack = new Stack<>();
     operatorStack = new Stack<>();
+  }
+  public void processor(){
+    Operator operator = operatorStack.pop();
+    Operand operand2 = operandStack.pop();
+    Operand operand1 = operandStack.pop();
+    operandStack.push(operator.execute(operand1, operand2));
   }
 
   public int evaluateExpression(String expression ) throws InvalidTokenException {
@@ -22,7 +28,7 @@ public class Evaluator {
 
     // The 3rd argument is true to indicate that the delimiters should be used
     // as tokens, too. But, we'll need to remember to filter out spaces.
-    this.expressionTokenizer = new StringTokenizer( expression, this.delimiters, true );
+    this.expressionTokenizer = new StringTokenizer(expression, this.delimiters, true);
 
     // initialize operator stack - necessary with operator priority schema
     // the priority of any operator in the operator stack other than
@@ -30,15 +36,14 @@ public class Evaluator {
     // of the usual operators
 
 
-
-    while ( this.expressionTokenizer.hasMoreTokens() ) {
+    while (this.expressionTokenizer.hasMoreTokens()) {
       // filter out spaces
-      if ( !( expressionToken = this.expressionTokenizer.nextToken() ).equals( " " )) {
+      if (!(expressionToken = this.expressionTokenizer.nextToken()).equals(" ")) {
         // check if token is an operand
-        if ( Operand.check( expressionToken )) {
-          operandStack.push( new Operand( expressionToken ));
+        if (Operand.check(expressionToken)) {
+          operandStack.push(new Operand(expressionToken));
         } else {
-          if ( ! Operator.check( expressionToken )) {
+          if (!Operator.check(expressionToken)) {
             throw new InvalidTokenException(expressionToken);
           }
 
@@ -47,25 +52,20 @@ public class Evaluator {
           // The Operator class should contain an instance of a HashMap,
           // and values will be instances of the Operators.  See Operator class
           // skeleton for an example.
-          Operator newOperator = new Operator();
-        
-          while (operatorStack.peek().priority() >= newOperator.priority() ) {
+          Operator newOperator = Operator.getOperator(expressionToken);
+
+          while (!operatorStack.isEmpty() && operatorStack.peek().priority() >= newOperator.priority()) {
             // note that when we eval the expression 1 - 2 we will
             // push the 1 then the 2 and then do the subtraction operation
             // This means that the first number to be popped is the
             // second operand, not the first operand - see the following code
-            Operator operatorFromStack = operatorStack.pop();
-            Operand operandTwo = operandStack.pop();
-            Operand operandOne = operandStack.pop();
-            Operand result = operatorFromStack.execute( operandOne, operandTwo );
-            operandStack.push( result );
+            processor();
           }
 
-          operatorStack.push( newOperator );
+          operatorStack.push(newOperator);
         }
       }
     }
-
 
     // Control gets here when we've picked up all of the tokens; you must add
     // code to complete the evaluation - consider how the code given here
@@ -75,7 +75,9 @@ public class Evaluator {
     // In order to complete the evaluation we must empty the stacks,
     // that is, we should keep evaluating the operator stack until it is empty;
     // Suggestion: create a method that processes the operator stack until empty.
-
-    return 0;
+    while (!operatorStack.isEmpty()) {
+      processor();
+    }
+    return operandStack.pop().getValue();
   }
 }
